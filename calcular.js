a9dd6d6fbc9adcf59bcf2bd2f10a1e5ba9a59a8f
Adrian GR <72535167+eydrien@users.noreply.github.com>
2024-10-25 19:45:43 -0500

    agregando estructuras

---
+++ calcular.js
@@ -0,0 +1,13 @@
+// Parseo en JavaScript
+var variable = parseFloat(document...('XXX').value) || 0;
+ 
+// Switch en JavaScript
+switch (op) {
+  case 'abc':
+    // código a efectuar
+    break;
+  case 'def':
+      // código a efectuar
+      break;
+  // ...
+}
