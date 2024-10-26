a8694acce82532fd12a59e78ed2e6081939ed808
Adrian GR <72535167+eydrien@users.noreply.github.com>
2024-10-25 20:02:34 -0500

    adicionar codigo

--- calcular.js
+++ calcular.js
@@ -1,13 +1,23 @@
 // Parseo en JavaScript
 var variable = parseFloat(document...('XXX').value) || 0;
- 
+
 // Switch en JavaScript
 switch (op) {
-  case 'abc':
-    // código a efectuar
-    break;
-  case 'def':
-      // código a efectuar
-      break;
-  // ...
+    case 'suma':
+        // código a efectuar
+        result = num1 + num2;
+        break;
+    case 'resta':
+        // código a efectuar
+        result= num1 - num2;
+        break;
+    case 'multiplicacion':
+        // código a efectuar
+        result = num1 * num2;
+        break;
+    case 'division':
+        // código a efectuar
+        result = num1 / num2;
+        break;
+    // ...
 }
