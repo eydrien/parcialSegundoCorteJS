a6eeef63bdfe32d5008e1847ef0d23673952f6ea
Adrian GR <72535167+eydrien@users.noreply.github.com>
2024-10-25 20:39:25 -0500

    finalizacion del ejercicio pero se actualizara mas a futuro

--- calcular.js
+++ calcular.js
@@ -1,32 +1,33 @@
-function calculate (op){
-// Parseo en JavaScript
-var num1 = parseFloat(document.getElementById('num1').value) || 0;
-var num1 = parseFloat(document.getElementById('num2').value) || 0;
+function calculate(op) {
+    // Parseo en JavaScript
+    var num1 = parseFloat(document.getElementById('num1').value) || 0;
+    var num2 = parseFloat(document.getElementById('num2').value) || 0;
 
-let result;
+    var result;
+    // Switch en JavaScript
+    switch (op) {
+        case 'suma':
+            // código a efectuar
+            result = num1 + num2;
+            break;
+        case 'resta':
+            // código a efectuar
+            result = num1 - num2;
+            break;
+        case 'multiplicacion':
+            // código a efectuar
+            result = num1 * num2;
+            break;
+        case 'division':
+            // código a efectuar
+            result = num1 / num2;
+            break;
+        default:
+            result = "ERROR jaja"
+        // ...
+    }
 
-// Switch en JavaScript
-switch (op) {
-    case 'suma':
-        // código a efectuar
-        result = num1 + num2;
-        break;
-    case 'resta':
-        // código a efectuar
-        result= num1 - num2;
-        break;
-    case 'multiplicacion':
-        // código a efectuar
-        result = num1 * num2;
-        break;
-    case 'division':
-        // código a efectuar
-        result = num1 / num2;
-        break;
-    default:
-        result = "ERROR jaja"
-    // ...
-}
     /* mostrar resultado */
     document.getElementById('result').value = result;
+
 }
